7355cded19a167248c34222217ceda78cefa8417
amylau68 <amy@framer.com>
2018-07-08 00:38:30 +0200

    Codecademy generated from the project submission page.

---
+++ Tian Yun Amy Liu - Capstone Project/code.sql
@@ -0,0 +1,116 @@
+WITH first_touch AS (
+    SELECT user_id,
+        MIN(timestamp) as first_touch_at
+    FROM page_visits
+    GROUP BY user_id)
+SELECT ft.user_id,
+    ft.first_touch_at,
+    pv.utm_source,
+		pv.utm_campaign
+FROM first_touch ft
+JOIN page_visits pv
+    ON ft.user_id = pv.user_id
+    AND ft.first_touch_at = pv.timestamp;
+
+
+
+
+SELECT COUNT(DISTINCT utm_campaign)
+FROM page_visits;
+
+SELECT COUNT(DISTINCT utm_source)
+FROM page_visits;
+
+SELECT DISTINCT utm_campaign,utm_source
+FROM page_visits;
+
+SELECT DISTINCT (page_name)
+FROM page_visits;
+
+WITH first_touch AS (
+    SELECT user_id,
+        MIN(timestamp) as first_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+ft_attr AS (
+		SELECT ft.user_id,
+    		 	 ft.first_touch_at,
+    		   pv.utm_source,
+				   pv.utm_campaign
+		FROM first_touch ft
+		JOIN page_visits pv
+    	ON ft.user_id = pv.user_id
+    	AND ft.first_touch_at = pv.timestamp
+  	)
+SELECT ft_attr.utm_source,
+			 ft_attr.utm_campaign,
+   		 count(*)
+FROM ft_attr
+GROUP BY 1, 2
+ORDER BY 3 desc;
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+lt_attr AS (
+		SELECT lt.user_id,
+    		 	 lt.last_touch_at,
+    		   pv.utm_source,
+				   pv.utm_campaign
+		FROM last_touch lt
+		JOIN page_visits pv
+    	ON lt.user_id = pv.user_id
+    	AND lt.last_touch_at = pv.timestamp
+  	)
+SELECT lt_attr.utm_source,
+			 lt_attr.utm_campaign,
+   		 count(*)
+FROM lt_attr
+GROUP BY 1,2
+ORDER by 3 desc;
+
+SELECT COUNT(DISTINCT user_id)
+FROM page_visits
+WHERE page_name = "4 - purchase";
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+  	WHERE page_name = "4 - purchase"
+    GROUP BY user_id),
+	lt_attr AS (
+		SELECT lt.user_id,
+    		   lt.last_touch_at,
+    		   pv.utm_source,
+			   pv.utm_campaign
+		FROM last_touch lt
+		JOIN page_visits pv
+    	ON lt.user_id = pv.user_id
+    	AND lt.last_touch_at = pv.timestamp
+  	)
+SELECT lt_attr.utm_source,
+	   lt_attr.utm_campaign,
+   	   count(*)
+FROM lt_attr
+GROUP BY 1,2
+ORDER by 3 desc;
+
+SELECT user_id, page_name, timestamp
+FROM page_visits
+ORDER BY user_id;
+
+SELECT user_id, count(DISTINCT page_name)
+FROM page_visits
+GROUP BY user_id
+ORDER BY 2 desc;
+
+
+
+
+
+
+    
+
